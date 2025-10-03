1ce15392300b5c5f2928214cfe6d4dfa71e9d769
Bruno Honda <bruno.honda@live.com>
2025-10-03 19:28:19 -0300

    feat: enhance feature toggle functionality with new UI components and logic

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,9 @@
 import { Component, Signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { FeatureToogle as FeatureToogleEnum } from './shared/enums/feature-toogle';
@@ -11,7 +14,10 @@ import { FeatureToogle } from './shared/services/feature-toogle';
   imports: [
     MatToolbarModule,
     MatButtonModule,
+    MatCardModule,
     MatIconModule,
+    MatSidenavModule,
+    MatSlideToggleModule,
     RouterLink,
     RouterOutlet,
   ],
@@ -19,11 +25,20 @@ import { FeatureToogle } from './shared/services/feature-toogle';
   styleUrl: './app.scss'
 })
 export class App {
+  public features = Object.values(FeatureToogleEnum);
   public isNewFeatureEnabled: Signal<boolean>;
 
   constructor(
-    _featureToogle: FeatureToogle
+    private readonly _featureToogle: FeatureToogle
   ) {
-    this.isNewFeatureEnabled = _featureToogle.getToogle(FeatureToogleEnum.newFeature);
+    this.isNewFeatureEnabled = this._featureToogle.getToogle(FeatureToogleEnum.newFeature);
+  }
+
+  isFeatureEnabled(feature: string): boolean {
+    return this._featureToogle.getToogle(feature as FeatureToogleEnum)();
+  }
+
+  updateFeatureToogle(feature: string, isEnabled: boolean): void {
+    this._featureToogle.setFeatureToogle(feature as FeatureToogleEnum, isEnabled);
   }
 }
