6cfb74e4b770375f69c8f17ee4e031aa104098e3
Bruno Honda <bruno.honda@live.com>
2025-10-03 09:27:32 -0300

    implements basic layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,17 @@
 import { Component, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterOutlet,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
