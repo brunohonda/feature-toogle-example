442fec1fc35a11921f2dc994d7af33ded5e60bd3
Bruno Honda <bruno.honda@live.com>
2025-10-03 18:57:53 -0300

    feat: implement feature toggle for new functionality in toolbar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { FeatureToogle as FeatureToogleEnum } from './shared/enums/feature-toogle';
+import { FeatureToogle } from './shared/services/feature-toogle';
 
 @Component({
   selector: 'app-root',
@@ -17,4 +19,11 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
+  public isNewFeatureEnabled: Signal<boolean>;
+
+  constructor(
+    _featureToogle: FeatureToogle
+  ) {
+    this.isNewFeatureEnabled = _featureToogle.getToogle(FeatureToogleEnum.newFeature);
+  }
 }
